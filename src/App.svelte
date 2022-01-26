88b7380d6a9781dc541851a59d4fc9233b59c259
kai <kai@Kais-MacBook-Pro.local>
2022-01-26 17:41:11 +0900

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	export let name;
+  import "./style/app.css";
+	import Tailwindcss from './Tailwindcss.svelte';
+  import Header from './Header.svelte';
+</script>
+<Tailwindcss/>
+
+
+<Header/>
+<main>
+<p class="text-6xl font-bold underline rainbow-text">
+	Hello {name}!
+</p>
+
+<p class='subscribe'>Subscribe My Youtube <a href="https://www.youtube.com/channel/UC4X8XQ6AMEHdVQOuMiEd5-A"><img alt='subscribe' src="/images/subcribe.png" class='inline-flex'/></a></p>
+
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	.subscribe {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+  }
+  .subscribe img {
+height:50px;
+}
+
+
+.rainbow-text {
+background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+ 
+  color: transparent;
+} 
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Header.svelte
@@ -0,0 +1,52 @@
+<script>
+</script>
+
+<!--
+<nav class="border-gray-200 px-2 sm:px-4 py-2.5 ">
+  <div class="px-8">
+  <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+<div class="flex-shrink-0 flex items-center">
+    <a title="logo" class="app_logo logo" href="/home">
+    <img src="/images/alkai_logo.png" class="w-10"/>
+    </a>
+</div>
+
+    <div class="sm:block sm:ml-6">
+    <div class="flex space-x-4">
+      <a href="!#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" x-state:on="Current" x-state:off="Default" aria-current="page" x-state-description="Current: &quot;bg-gray-900 text-white&quot;, Default: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">Dashboarpd알려준다카이 ENSv</a>
+    </div>
+    </div>
+    </div>
+</div>
+</nav>
+-->
+<nav  class="bg-gray-800">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div class="relative flex items-center justify-between h-16">
+        <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex-shrink-0 flex items-center">
+            <img class="lg:block h-10 w-auto" src="/images/alkai_logo.png" alt="Workflow">
+          </div>
+          <div class="sm:block sm:ml-6">
+            <div class="flex space-x-4">
+                <a href="!#" class="text-white bg-gray-700 px-3 py-2 rounded-md text-sm font-medium menu-component">알려준다카이 ENS</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </nav>
+
+
+<style>
+nav{
+background: #f4f5f8;
+height:70px;
+}
+
+.menu-component {
+  margin-top: 3px;
+}
+
+</style>
